2218a00ba2a4cbdaeb2d27666f9f39226ab71cff
hypnas200114 <tets03@uqat.ca>
2025-03-10 10:47:47 -0400

    fonctionnalBalls
    fonctionnalBalls

--- Assets/Scrips/CameraMovement.cs
+++ Assets/Scrips/CameraMovement.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    [Header("Informations")]
+    public Transform positionJoueur; //Positionnement du personnage
+
+    Camera mCamera;
+    float timeTransition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        mCamera = GetComponent<Camera>(); //Prend la camera
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeTransition += Time.deltaTime;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(positionJoueur.position.x, positionJoueur.position.y, transform.position.z), timeTransition);
     }
 }

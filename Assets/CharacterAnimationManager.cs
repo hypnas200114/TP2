f0c527bebe2b66a8a226d3484fc7657148504e9b
hypnas200114 <tets03@uqat.ca>
2025-03-10 21:19:07 -0400

    FinalChanges
    FinalChanges

--- Assets/CharacterAnimationManager.cs
+++ Assets/CharacterAnimationManager.cs
@@ -2,17 +2,42 @@ using UnityEngine;
 
 public class CharacterAnimationManager : MonoBehaviour
 {
+    [Header("AudioClips")]
+    public AudioClip pleur;
+    public AudioClip musique;
+
+
+    //Initialise les valeurs prive
     Animator animator;
+    AudioSource audioSource;
+    ParticleSystem ps;
+
+    bool hasBeenHit = false;
 
     private void Start()
     {
+        //Initialise les valeurs et va les chercher dans le personnage
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        ps = GetComponent<ParticleSystem>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        //Effectue l'animation de douleur
         animator.SetTrigger("Impact");
-        Debug.Log("Ouch");
+
+        //Effectue l'effet de particule
+        ps.Play();
+
+        //si n'a jamais ete toucher
+        if (!hasBeenHit) { 
+        //change la musique pour des pleures
+        audioSource.resource = pleur;
+        audioSource.loop = true;
+        audioSource.Play();
+        hasBeenHit = true;
+        }
     }
 
 }
